f962fde076be6a5cc37a5f6d52896c286c63c6c9
Russ Johnson <rusjohnson@ebay.com>
2015-06-23 16:14:26 +0200

    session now stored in redis

--- ui/src/main/java/demo/UiApplication.java
+++ ui/src/main/java/demo/UiApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
+@EnableRedisHttpSession
 public class UiApplication {
 
     @RequestMapping("/user")
